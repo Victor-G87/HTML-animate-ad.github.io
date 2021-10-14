5cd68c384b53c6e9c7c581d28ba1d224caf66341
Victor-G87 <victor.g.hernandez87@gmail.com>
2021-10-14 02:00:44 -0500

    updates 2

--- cubeism.js
+++ cubeism.js
@@ -3693,7 +3693,7 @@ p.nominalBounds = new cjs.Rectangle(0,0,713.3,224.1);
 
 // stage content:
 (lib.cubeism = function(mode,startPosition,loop,reversed) {
-if (loop == null) { loop = false; }
+if (loop == null) { loop = true; }
 if (reversed == null) { reversed = false; }
 	var props = new Object();
 	props.mode = mode;
